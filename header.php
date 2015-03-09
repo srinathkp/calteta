424cf25414aa56785fb705ee87e8ff15eec27eac
kousiksatish <kousiksatish@gmail.com>
2015-03-09 17:53:01 +0530

    Basic SB Admin theme added

---
+++ header.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Calteta">
+<meta name="author" content="Calteta Dev Team">
+
+<link href="theme/css/bootstrap.min.css" rel="stylesheet">
+
+<link href="theme/css/sb-admin-2.css" rel="stylesheet">
---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
